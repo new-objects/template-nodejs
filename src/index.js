1c501692253482be351a3efcf09b4cae240f3af5
mi-skam <maksim.codes@mailbox.org>
2023-11-16 14:15:03 +0100

    index.js: wasn't compatible with Windows

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
 
 // get the current directoryof the current script without the file name
-const __dirname = new URL(".", import.meta.url).pathname;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const root = __dirname.replace("src", "");
+const publicPath = path.join(root, "public");
 
-app.use(express.static(root + "/public"));
-app.get("/", (req, res) => res.sendFile("index.html"));
+app.use(express.static(publicPath));
+
+app.get("/", (_, res) => res.sendFile(path.join(publicPath, "index.html")));
+console.log(publicPath);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
